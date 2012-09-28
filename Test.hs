bce2c5733b5347e67633529b1113f4a0665548e5
Takayuki Muranushi <muranushi@gmail.com>
2012-09-29 08:42:13 +0900

    even more severe incline

--- Test.hs
+++ Test.hs
@@ -1,24 +1,32 @@
 import Numeric.Optimization.Algorithms.CMAES
 
 -- a function with minimum at x[i]=sqrt(i)
-f xs = sum $ zipWith (\x i -> abs $ x**2-i) xs [1..]
+f xs = sum $ zipWith g xs [1..]
+  where
+    g x i = (x * abs x - i)**2
 
 -- a function with scale diversity
-f2 xs = sum $ zipWith (\x i -> abs $ (x/1e4**i)**2-i ) xs [1..]
+f2 xs = sum $ zipWith g xs [1..]
+  where
+    g x' i = let x = x'/1e10**i in
+      (x * abs x - i)**2
 
 main = do
   -- this works fine
   xs <- run $ minimizer [1..10] f
   print xs
+  print $ f xs
 
   -- if the scale for parameters differ too much
   -- it doesn't work very well
   xs <-run $ minimizer [1..10] f2
   print xs
+  print $ f2 xs
 
-  -- you need to hint the mimimizer with the
+  -- you need to hint the optimizer with the
   -- appropriate scales
   xs <- run $
          (minimizer [1..10] f2)
-           {scaling = Just [1e4**i | i<-[1..10] ] }
+           {scaling = Just [1e10**i | i<-[1..10] ] }
   print xs
+  print $ f2 xs
